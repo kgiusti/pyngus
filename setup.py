6943b8d1b157aecca72cf7a30db878a4fa3bcadb
Kenneth Giusti <kgiusti@redhat.com>
2018-07-13 16:37:03 -0400

    Various fixes
    1) remove upper constraint on python-qpid-proton.  The proton core API
    has been stable for a couple years now and I continually test Pyngus against the latest Proton releases.
    
    2) remove the hack to fix distro packages with bad dependencies.
    Complain to the distro packager if they haven't included a proper
    dependency on python-qpid-proton in their Pyngus distro configuration.
    
    3) keytool has stopped working for me... so buh-bye keytool I'm
    sticking with openssl...

--- setup.py
+++ setup.py
@@ -22,21 +22,6 @@ from setuptools import setup
 
 _VERSION = "2.2.4"   # NOTE: update __init__.py too!
 
-# I hack, therefore I am (productive) Some distros (which will not be named)
-# don't use setup.py to install the proton python module.  In this case, pip
-# will not think proton is installed, and will attempt to install it,
-# overwriting the distro's installation.  To prevent this, don't set the
-# 'install_requires' if the proton python module is already installed
-#
-_dependencies = []
-try:
-    import proton
-except ImportError:
-    # this version of proton will download and install the proton shared
-    # library as well:
-    _dependencies = ['python-qpid-proton>=0.9,<0.24']
-
-
 setup(name="pyngus",
       version=_VERSION + os.environ.get('PYNGUS_VERSION_SUFFIX', ''),
       author="kgiusti",
@@ -46,7 +31,7 @@ setup(name="pyngus",
       description="Callback API implemented over Proton",
       url="https://github.com/kgiusti/pyngus",
       license="Apache Software License",
-      install_requires=_dependencies,
+      install_requires=['python-qpid-proton'],
       classifiers=["License :: OSI Approved :: Apache Software License",
                    "Intended Audience :: Developers",
                    "Operating System :: OS Independent",
--- tests/unit_tests/ssl_db/setup_ssl.sh
+++ tests/unit_tests/ssl_db/setup_ssl.sh
@@ -40,37 +40,24 @@
 #set -x
 
 OPENSSL=$(type -p openssl)
-if [[ !(-x $OPENSSL) ]] ; then
+if [[ ! -x $OPENSSL ]] ; then
     echo >&2 "'openssl' command not available, certificates not generated"
-    exit 0
-fi
-KEYTOOL=$(type -p keytool)
-if [[ !(-x $KEYTOOL) ]] ; then
-    echo >&2 "'keytool' command not available, certificates not generated"
     exit 0
 fi
 
 # clean up old stuff
-rm -f *.pem *.pkcs12
+rm -f *.pem
 
-CA_COMMON_NAME="example.ca.com"
 # Create a self-signed certificate for the CA, and a private key to sign certificate requests:
-$KEYTOOL -storetype pkcs12 -keystore ca.pkcs12 -storepass ca-password -alias ca -keypass ca-password -genkey -dname "CN=$CA_COMMON_NAME" -validity 9999
-$OPENSSL pkcs12 -nokeys -passin pass:ca-password -in ca.pkcs12 -passout pass:ca-password -out ca-certificate.pem
+openssl req -x509 -out ca-certificate.pem -days 3650 -nodes -newkey rsa:2048 -keyout ca-private-key.pem -subj "/CN=example.ca.com" -passout pass:ca-password
 
-SERVER_COMMON_NAME="*.server.com"
-# Create a certificate request for the server certificate.  Use the CA's certificate to sign it:
-$KEYTOOL -storetype pkcs12 -keystore server.pkcs12 -storepass server-password -alias server-certificate -keypass server-password -genkey  -dname "CN=$SERVER_COMMON_NAME" -validity 9999
-$KEYTOOL -storetype pkcs12 -keystore server.pkcs12 -storepass server-password -alias server-certificate -keypass server-password -certreq -file server-request.pem
-$KEYTOOL -storetype pkcs12 -keystore ca.pkcs12 -storepass ca-password -alias ca -keypass ca-password -gencert -rfc -validity 99999 -infile server-request.pem -outfile server-certificate.pem
-$OPENSSL pkcs12 -nocerts -passin pass:server-password -in server.pkcs12 -passout pass:server-password -out server-private-key.pem
+# Create a certificate for the server.  Use the CA's certificate to sign it:
+openssl req -out server-request.csr -days 3650 -newkey rsa:2048  -keyout server-private-key.pem -subj "/CN=*.server.com/OU=812" -passout pass:server-password
+openssl x509 -req -in server-request.csr -CA ca-certificate.pem -CAkey ca-private-key.pem -CAcreateserial -out server-certificate.pem -days 3650
 
-CLIENT_COMMON_NAME="my.client.com"
-# Create a certificate request for the client certificate.  Use the CA's certificate to sign it:
-$KEYTOOL -storetype pkcs12 -keystore client.pkcs12 -storepass client-password -alias client-certificate -keypass client-password -genkey  -dname "CN=$CLIENT_COMMON_NAME" -validity 99999
-$KEYTOOL -storetype pkcs12 -keystore client.pkcs12 -storepass client-password -alias client-certificate -keypass client-password -certreq -file client-request.pem
-$KEYTOOL -storetype pkcs12 -keystore ca.pkcs12 -storepass ca-password -alias ca -keypass ca-password -gencert -rfc -validity 99999 -infile client-request.pem -outfile client-certificate.pem
-$OPENSSL pkcs12 -nocerts -passin pass:client-password -in client.pkcs12 -passout pass:client-password -out client-private-key.pem
+# Create a certificate for the client.  Use the CA's certificate to sign it:
+openssl req -out client-request.csr -days 3650 -newkey rsa:2048 -keyout client-private-key.pem -subj "/CN=my.client.com/OU=812" -passout pass:client-password
+openssl x509 -req -in client-request.csr -CA ca-certificate.pem -CAkey ca-private-key.pem -CAcreateserial -out client-certificate.pem -days 3650
 
 # clean up all the unnecessary stuff
-rm *.pkcs12 *-request.pem
+rm -f *.csr *.srl
